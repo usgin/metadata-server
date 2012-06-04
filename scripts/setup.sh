1850b5da9f990dc6a8f089ad056d475335d195d6
Ryan Clark <ryan.clark.j@gmail.com>
2012-06-04 13:16:06 -0500

    Adjustment to setup to pull in the submodule

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-if [ ! -e couchdb-lucene/target/couchdb-lucene-0.8.0-dist.zip ]; then
+
+if [ ! "$(ls -A couchdb-lucene)" ]; then
+	git submodule init
+	git submodule update
 	cd couchdb-lucene
 	mvn
 	cd target
